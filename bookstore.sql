4cd9ca32da28f0af7c53f9ff4afba5792419b576
Vannexs <vickyannexiss@gmail.com>
2025-04-12 21:15:30 +0300

    added queries for creating tables that were assigned

--- bookstore.sql
+++ bookstore.sql
@@ -1,1 +1,11 @@
 CREATE DATABASE bookstore;
+
+CREATE TABLE booktable;
+
+CREATE TABLE book_authortable;
+
+CREATE TABLE book_languagetable;
+
+CREATE TABLE authortable;
+
+CREATE TABLE publishertable;
