b51c2d12db7b3b6dd8a839e33a0c829a94693d47
Vannexs <vickyannexiss@gmail.com>
2025-04-13 22:16:49 +0300

    removed table from table titles

--- bookstore.sql
+++ bookstore.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE bookstore;
 
-CREATE TABLE booktable(
+CREATE TABLE book(
     book_id PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     FOREIGN KEY publisher_id REFERENCES publishertable(publisher_id),
@@ -9,24 +9,24 @@ CREATE TABLE booktable(
     publication_date DATE NOT NULL,
 );
 
-CREATE TABLE book_authortable(
+CREATE TABLE book_author(
     FOREIGN KEY book_id REFERENCES booktable(book_id),
     FOREIGN KEY author_id REFERENCES authortable(author_id),
 );
 
-CREATE TABLE book_languagetable(
+CREATE TABLE book_language(
     language_id PRIMARY KEY,
     language_name VARCHAR(100) NOT NULL,
 );
 
-CREATE TABLE authortable(
+CREATE TABLE author(
     author_id PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     bio VARCHAR(500) NOT NULL,
 );
 
-CREATE TABLE publishertable(
+CREATE TABLE publisher(
     publisher_id PRIMARY KEY,
     publisher_name VARCHAR(100) NOT NULL,
     contact_number VARCHAR(15) NOT NULL,
